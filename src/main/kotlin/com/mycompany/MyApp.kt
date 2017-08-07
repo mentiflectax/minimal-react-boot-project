51c50a4ab28fcdeef4219aa26376b5ae266d3ede
dp118m <dp@altruix.co>
2017-08-07 22:39:25 +0300

    Attempt to fix the error by changing RestController to Controller.

--- src/main/kotlin/com/mycompany/MyApp.kt
+++ src/main/kotlin/com/mycompany/MyApp.kt
@@ -1,11 +1,10 @@
 package com.mycompany
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
 
 @SpringBootApplication
-@RestController
+@Controller
 open class MyApp {
 
 }
