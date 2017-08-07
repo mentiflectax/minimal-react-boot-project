bdb8a0ca937f78764cb3690ff2da5befc28af824
dp118m <dp@altruix.co>
2017-08-07 22:43:38 +0300

    Attempt to fix the error by moving the main method into the app class body.

--- src/main/kotlin/com/mycompany/MyApp.kt
+++ src/main/kotlin/com/mycompany/MyApp.kt
@@ -1,10 +1,13 @@
 package com.mycompany
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.stereotype.Controller
 
 @SpringBootApplication
 @Controller
 open class MyApp {
-
+    fun main(args: Array<String>) {
+        SpringApplication.run(MyApp::class.java,*args)
+    }
 }
