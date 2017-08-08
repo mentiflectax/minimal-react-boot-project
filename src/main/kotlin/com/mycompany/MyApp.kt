c15232867a60457d0c70f6c2910ace166c8ce869
dp118m <dp@altruix.co>
2017-08-08 14:31:15 +0300

    Fixed the 404 error.

--- src/main/kotlin/com/mycompany/MyApp.kt
+++ src/main/kotlin/com/mycompany/MyApp.kt
@@ -2,11 +2,21 @@ package com.mycompany
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+
+
 
 @SpringBootApplication
 @Controller
 open class MyApp {
+    @Bean
+    open fun yourTemplateResolver(): ClassLoaderTemplateResolver {
+        val yourTemplateResolver = ClassLoaderTemplateResolver()
+        yourTemplateResolver.prefix = "templates/"
+        return yourTemplateResolver
+    }
     fun main(args: Array<String>) {
         SpringApplication.run(MyApp::class.java,*args)
     }
